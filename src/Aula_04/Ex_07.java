b389918413f36120466cc65010ec803d0ad9476e
dmansoldo@gmail.com <diego.amalmeida@CAS555488W1030.senacsp.edu.br>
2019-09-04 21:57:58 -0300

    Número primo com recursividade

--- src/Aula_04/Ex_07.java
+++ src/Aula_04/Ex_07.java
@@ -21,24 +21,41 @@ public class Ex_07 {
     }
 
     public static boolean ePrimo(int n) {
-        
-        for (int i = 2; i <= n; i++) {
+
+        for (int i = 2; i < n; i++) {
 
             if (n % i == 0) {
-               return false;
+                return false;
             }
         }
         return true;
+    }
+
+    public static boolean ePrimoRecursivo(int n) {
+
+        return ePrimoRec(n, n - 1);
+    }
+    
+    public static boolean ePrimoRec(int n, int i) {
+        
+        if(i == 1){
+            return true;
+        }
+        
+        if(n%i == 0){
+            return false;
+        }
+        return ePrimoRec(n, i - 1);
     }
 
     public static void main(String[] args) {
 
         int a = solicitaNum();
 
-        if(ePrimo(a)){
-             System.out.println("Número primo!");
+        if (ePrimoRecursivo(a)) {
+            System.out.println("Número primo!");
         } else {
             System.out.println("Não é número primo!");
-        }     
+        }
     }
 }
